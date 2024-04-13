bb23cd424fe393183117684e7d948057881a1a40
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-04-13 10:16:59 +0000

    a very few changes in first file and second file is based on datatypes conversions and operations

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,15 @@
-console.log("Isha jai singh");
+//this script is for first very basics of javascript //
+//variables and constant
+const accountId = 199876;
+let accountemail= "ishagoogle@gmail.com ";
+var accountPassword= 12345
+accountCity= "jaipur"
+let accountState;
+// accountId=80
+//console.log(accountId);
+ accountemail= "ijs@ijs.com ";
+  accountPassword= 45674
+accountCity= "California"
+
+//console.table([accountId, accountPassword , accountemail, accountCity , accountState])
+
---
+++ 01_basics/second.js
@@ -0,0 +1,28 @@
+"use strict"; // this will treat any js code as newer version of js ( js-> javascript)
+ //let score = "23"
+ /*console.log(typeof score)
+
+ console.log(typeof (score))*/
+/* let valueInNumber = Number(score)
+console.log(typeof valueInNumber) */
+ /* let unit ="33abx"
+  let StringInNumber= Number(unit)
+  console.log( StringInNumber)
+  console.log(  typeof StringInNumber)
+ console.log( unit)
+ console.log(typeof unit)
+
+  let name1 = "Isha ";
+let name2 = "Shains";
+console.log( name1 )
+console.log(typeof name1 )
+
+console.log( name2 )
+console.log( typeof name2 )
+ */
+/*let number = 33
+let stringNumber = String(number)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)*/
+ //******************operations****************
+ 
