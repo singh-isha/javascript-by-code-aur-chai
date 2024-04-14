b5ccff8ddfcc9a2a6311b168a61313f023044d9a
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-04-14 14:35:30 +0000

    morethan basics and all about datatypes ( basic discussions)

---
+++ 01_basics/third.js
@@ -0,0 +1,19 @@
+// summary of different datatypes which we studied till now 
+const id = Symbol( '197')
+const Newid = Symbol( '197')
+ //console.log( id==Newid)
+//const BigNumber = 29883746892754757n
+//console.log( typeof BigInt)
+ const array =[ 'cleanser ' , 'serum ','moisturizer ', 'sunscreen' ]
+let object ={
+    name:"Isha",
+    age :22,
+
+}
+//console.log(typeof array)
+//console.log( typeof object)
+const myfunction = function(){
+    console.log( " hieee")
+}
+//console.log( typeof myfunction)
+// read this section from ecma script ( sources : online)
