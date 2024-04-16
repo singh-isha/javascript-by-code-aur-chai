ec220b18225ee0956c65d02fff10844bc9030aa7
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-04-16 14:46:39 +0000

    all about maths and number theory

--- 01_basics/fourth.js
+++ 01_basics/fourth.js
@@ -3,5 +3,19 @@ const score = 220
 //console.log(score)
 const balance  = new Number(890);// specially shows the type of any given arguments
 //console.log( balance)
-console.log( balance .toString().length)
-console.log( balance .toFixed(5))
+//console.log( balance .toString().length)
+//console.log( balance .toFixed(5))
+ // all about maths 
+ console.log ( Math);
+ console.log ( Math.abs(-4));//  it will not convert positive into negative, positive will remain positive 
+ console.log ( Math.round(6.397));
+ console.log ( Math.floor(0.999));
+ console.log ( Math.ceil(2.336));
+ console.log ( Math.min(0.1 , -2, 1, -100));
+ console.log ( Math.max(0.11, -5, 0.001 , 2**-1))
+ //console.log ( Math.random());
+ //console.log ( Math.random());
+
+ //console.log ( Math)// will use later to show combinational use of different properties of maths liberary 
+ //console.log ( Math)
+
