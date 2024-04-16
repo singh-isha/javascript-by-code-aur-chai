fa842b50933307b8448ecc161565e3c5f14544ae
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-04-16 06:29:19 +0000

    all about numbers and maths

---
+++ 01_basics/fourth.js
@@ -0,0 +1,7 @@
+// all about numbers and maths 
+const score = 220
+//console.log(score)
+const balance  = new Number(890);// specially shows the type of any given arguments
+//console.log( balance)
+console.log( balance .toString().length)
+console.log( balance .toFixed(5))
