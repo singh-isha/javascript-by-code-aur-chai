d5605a4dad882e83063b4f0cbd00306b4b661886
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-04-20 08:38:16 +0000

    all about arrays ( not complete yet)

---
+++ 01_basics2/second.js
@@ -0,0 +1,28 @@
+// this repo is all about Array ( from basics to Advance)
+//methods of decalaring arrays
+const array=[1,2,3,4,5]
+//for character array 
+//const character=["isha"," priyanshi " ," keshav " , " vaman ","  siddhant"]
+//decalaring array by the method of object
+//const array1 =new array( 1,2,3,4,5,);
+//console.log(array1)
+//for accessing array we use indexes 
+//methods of array=> push , pop ,shift , unshift
+//const array=[1,2,3,4,5]
+//array.push(99)
+//array.push(8)
+//array.pop(4)
+ //console.log(array);
+ //console.log(array.includes(23));
+ //console.log(array.indexOf(4));
+ //console.log(array.findIndex(55));
+ //console.log(array.);
+ //console.log(array);
+const fruits=["apple","mango","orange", "pear"]
+const flowers=["rose","sunflower","daisy","lily"]
+fruits.push(flowers);
+  console.log(fruits)
+  //console.log()
+  
+  
+
