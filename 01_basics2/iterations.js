0ea510eeb6c39c98a326767963aadcbc8e587c2c
Isha singh <116877229+aein213097@users.noreply.github.com>
2024-04-26 22:40:09 +0530

    this repo is all about iterative loops

---
+++ 01_basics2/iterations.js
@@ -0,0 +1,24 @@
+// this file is all about iterations means for loops , while loops and do-while loops
+// for loop
+/* let n = 10;
+for (let i  = 1;i  <= n ;i ++) {
+    const element = i;
+    console.log(element);
+} */
+//whiile loop
+ /* let n =0;
+while( n<=10) {
+    console.log( n)
+    n= n+1
+} */
+// do while loops
+ /* let n =11
+ do{
+ console.log(n)
+  n=n+1
+ }while (n <=10) 
+ */
+
+
+    
+ 
