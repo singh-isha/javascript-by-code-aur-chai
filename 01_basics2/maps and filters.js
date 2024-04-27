3181c41a73aacc955d7b2ed37538ef14110299bd
Isha singh <116877229+aein213097@users.noreply.github.com>
2024-04-27 12:33:49 +0530

    this file is all about maps , filters and reduce

---
+++ 01_basics2/maps and filters.js
@@ -0,0 +1,14 @@
+// this repo is all about high order array loops, maps and filters also reduce is included in this repo 
+// this completes the whole part-1 of javascript in code with chai..... so ALL THE BEST FOR NEXT PART  and i hope aapne bjhot kuch sikha hoga is part mei .
+
+/* const map=new Map();
+map.set( '@', "apple")
+map.set( 'S', "samsung")
+map.set( '1+', "one plus")
+map.set( 'V', "vivo")
+map.set( '@', "apple")
+map.set( 'Mi', "redmi")
+console.log(map) */
+  //NOTE :this shows us that in maps they don't show repated and duplicate values , one value can be printed only once 
+  // in for of loops they show keys in case of objects and for of loops they show values by default if you just say console by add them in objects to print 
+  
