fcb58a9a881e0008ee256ccf10bbc3c9357eebc6
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-05-14 17:48:34 +0000

    all about fetch

---
+++ advance_javascript/3.js
@@ -0,0 +1,13 @@
+// all about fetch 
+/*
+The global fetch() method starts the process of fetching a resource from the network, returning a promise that is fulfilled once the response is available.
+
+The promise resolves to the Response object representing the response to your request.
+
+A fetch() promise only rejects when the request fails, for example, because of a badly-formed request URL or a network error. A fetch() promise does not reject if the server responds with HTTP status codes that indicate errors (404, 504, etc.). Instead, a then() handler must check the Response.ok and/or Response.status properties.
+
+The fetch() method is controlled by the connect-src directive of Content Security Policy rather than the directive of the resources it's retrieving.
+
+
+
+*/
