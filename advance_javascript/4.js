d44c919df484f17400577da5bfc1aa3ef4b3ef90
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-05-19 15:03:23 +0000

    this is all about classes

---
+++ advance_javascript/4.js
@@ -0,0 +1,30 @@
+/* const user = {
+    username :"Isha",
+     signedIn:true,
+      loggedIn :5,
+       getUsersDetail: function(){
+        console.log( " your details are here"); */
+        // console.log(` username:${this.username}`); // this key word refers to current instances or can say which particular statement you are refering to get print in this sequeal 
+        //console.log(this);
+      // }
+//}
+//console.log(user.username);
+//console.log(user.getUsersDetail());
+//console.log(this);
+/*  function User (username , loggedIn, signedIn ){
+this.username= username;
+
+this.loggedIn= loggedIn;
+
+this.signedIn= signedIn;
+
+// return this :-> implicitly define hota h this keyword 
+ }
+ const user1= new  User("isha " , "jaisingh.com" , true);
+ const user2= new  User("keshav " , "keshav.com" , true);
+ const user3= new  User("priya " , "priyanshi.com" , false);
+ console.log(user1);
+ console.log(user2);
+ console.log(user3);
+  */
+ 
