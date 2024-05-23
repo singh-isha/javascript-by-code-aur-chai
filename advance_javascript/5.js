1919b0e4d068f924e96289cc596d9d6e131d2f35
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-05-23 11:21:25 +0000

    object and new keyword

---
+++ advance_javascript/5.js
@@ -0,0 +1,30 @@
+/* // this repo is all about understanding of object and new keyword in it 
+function multiplyBy5(num){
+     return num*5
+}
+multiplyBy5.power =2
+console.log( multiplyBy5(6));
+console.log( multiplyBy5.power);
+console.log( multiplyBy5.prototype);
+ */
+/* function createUser(username, point){
+     this.username= username
+     this.point= point
+
+}
+ createUser.prototype.increment= function(){
+     point++
+ }
+ createUser.prototype.show= function(){
+     console.log(`price given to  ${this.point}`);
+ }
+  
+ const coffee = new createUser("coffee" , 100)
+ const starbucks = new createUser("starbucks" , 800)
+coffee.show()
+starbucks.show()
+ */
+
+ 
+
+
