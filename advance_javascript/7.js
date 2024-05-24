cf3ed609a2de44bdbef7ce502cd36a35bd8f8715
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-05-24 17:48:15 +0000

    bmi calculator js code -> js code

---
+++ advance_javascript/7.js
@@ -0,0 +1,20 @@
+// this repo is all about  call and this keyword their usage and different ways to use them in different secenarioes
+function SetUserName (username){
+this.username = username
+}
+ function createUsername ( username , email ,password){
+//SetUserName(username)
+ this.username = username
+ this.email  = email
+ this.password = password
+ }
+  const createuser = new createUsername( "isha" , " ishasingh.com" , "1123")
+   console.log(createuser);
+
+
+
+
+
+
+
+   
