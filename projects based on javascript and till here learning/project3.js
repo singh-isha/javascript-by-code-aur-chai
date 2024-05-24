dff43a7dead2f159b94f7baf6644b57495897ba0
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-05-24 17:51:03 +0000

    digital clock generator

---
+++ projects based on javascript and till here learning/project3.js
@@ -0,0 +1,11 @@
+// Digital clock : which shows your current time 
+
+const clock = document.getElementById('clock');
+// const clock = document.querySelector('#clock')
+
+setInterval(function () {
+  let date = new Date();
+  // console.log(date.toLocaleTimeString());
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
+
