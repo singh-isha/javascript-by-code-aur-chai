d45f40a3d7b3dcacbdee007977cb508da23bb35a
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-05-26 03:52:35 +0000

    about call and using this keyword how can we use it

--- advance_javascript/7.js
+++ advance_javascript/7.js
@@ -1,17 +1,17 @@
 // this repo is all about  call and this keyword their usage and different ways to use them in different secenarioes
 function SetUserName (username){
 this.username = username
+ console.log("called");
 }
  function createUsername ( username , email ,password){
-//SetUserName(username)
- this.username = username
+  SetUserName.call(this,username)
+ //this.username = username
  this.email  = email
  this.password = password
  }
   const createuser = new createUsername( "isha" , " ishasingh.com" , "1123")
    console.log(createuser);
-
-
+ 
 
 
 
