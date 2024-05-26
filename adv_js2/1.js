2763904037765bcea54c159ea2bb4e0aeaab3d6c
Isha singh <165162814+singh-isha@users.noreply.github.com>
2024-05-26 10:02:01 +0000

    all about class constructor and static

---
+++ adv_js2/1.js
@@ -0,0 +1,37 @@
+/* class user{
+     constructor( username , email , password ){
+        this.username  = username ;
+        this.password  = password ;
+        this.email  = email ;
+         
+     }
+      encryptPassword(){
+        return`${this.password } sdfg`
+      }
+      changeUserName(){
+       return`${this.username.toUpperCase()}`
+      }
+}
+
+
+ const coffee = new user( "coffee" , "coffee@gmail.com" , " 12344")
+  //console.log(coffee);
+  console.log(coffee.encryptPassword());
+
+
+    console.log(coffee.changeUserName()); */
+    // what is happenning in javascript engine or can say at lowest level of working in language  at this point ::
+    /*  function User(username , email , password){
+      this.username  = username ;
+      this.password  = password ;
+      this.email  = email ;
+     }
+      User.prototype.encryptPassword= function(){
+        return`${this.password }opps`
+}
+      const coffee = new User( "coffee" , "coffee@gmail.com" , " 12344")
+      console.log(coffee.encryptPassword()); */
+       /* NOTE: same goes to change username in uppercase letters 
+        all this is happening is showing that what exactly happen at behind the secens  of all this code which we write just to change and uppercase any thing realted to users  */
+
+        
